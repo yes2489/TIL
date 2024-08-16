0e58ede5e682166558db41c9f4f129b612b1f1f1
yes2489 <yes24150@gmail.com>
2024-08-16 16:48:09 +0900

    240816

---
+++ 신입사원 분반 교육/src/Solution2.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution2 {
+	static List<Integer> score1;
+	static List<Integer> score2;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int TC = sc.nextInt();
+		
+		for (int test = 1; test <= TC; test++) {
+			int N = sc.nextInt(); // 학생 수
+			int min = sc.nextInt(); // 최소 인원
+			int max = sc.nextInt(); // 최대 인원
+			int[] arr = new int [N];
+			
+			for (int n = 0; n < N; n++) {
+				arr[n] = sc.nextInt();
+			}// 입력 끝
+			
+			
+			
+			
+		}
+
+	}
+
+}
